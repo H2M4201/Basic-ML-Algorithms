b6655c9c0ff051447e84092231fee04d445bef03
H2M4201 <55888616+H2M4201@users.noreply.github.com>
2022-05-06 22:52:10 +0700

    Add files via upload

---
+++ Naive_Bayes.py
@@ -0,0 +1,115 @@
+from math import exp, pi, sqrt
+from random import random, randint
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_data(n, p, K):
+    data = np.zeros((p + 1, n))
+    for i in range (n):
+        for j in range (p):
+            data[j, i] = randint(-300, 300) * random()
+            data[p, i] = randint(1, K)
+    return data
+
+def counting_class_size(data, K):
+    n = len(data[0])
+    p = len(data) - 1
+    count = np.zeros(K, int)
+    for i in range (n):
+        count[int(data[p, i]) - 1] +=1
+    return count
+
+def estimate_prior_prob(data, count, K):
+    n = len(data[0])
+    tmp = np.zeros(K)
+    for i in range (K):
+        tmp[i] = count[i] / n
+    return tmp
+
+def mean_by_class_and_predictor(data, count, K):
+    n = len(data[0])
+    p = len(data) - 1
+    mean = np.zeros((p, K))
+    for i in range (p):
+        for j in range (n):
+            mean[i, int(data[p, j]) - 1] += data[i, j]
+    
+    for i in range (p):
+        for j in range (K):
+            if (count[j] > 0):
+                mean[i, j] /= count[j]
+
+    return mean
+
+def variance_by_class_and_predictor(data, mean, K):
+    n = len(data[0])
+    p = len(data) - 1
+    variance = np.zeros((p, K))
+    count = np.zeros(K)
+    for i in range (p):
+        for j in range (n):
+            variance[i, int(data[p, j]) - 1] += (data[i, j] - mean[i, int(data[p, j]) - 1]) ** 2
+            if i == 0:
+                count[int(data[p, j]) - 1] += 1
+    
+    for i in range (p):
+        for j in range (K):
+            if (count[j] > 0):
+                variance[i, j] /= count[j]
+
+    return variance
+
+def Gaussian_probability_density (x, mean, sd):
+    if (sd == 0):
+        sd = 0.0000001
+    a = -(x - mean) * (x - mean)
+    b = 2 * sd * sd
+    return np.float64(exp(a / b) / (sd * sqrt(2 * pi)))
+
+def naive_bayes_classification (data, K):
+    n = len(data[0])
+    p = len(data) - 1
+    count = counting_class_size(data, K)
+    prior_prob = estimate_prior_prob(data, count, K)
+    mean = mean_by_class_and_predictor(data, count, K)
+    v = variance_by_class_and_predictor(data, mean, K)
+    res = np.zeros(n)
+    for i in range (n):
+        classify_result = np.zeros(K)
+        s = 0.0
+        for j in range (K):
+            tmp = 1.0
+            for k in range (p):
+                sd = sqrt(v[k, j])
+                if (sd == 0):
+                    sd = 0.0001
+                tmp *= Gaussian_probability_density( \
+                    data[k, i], mean[k ,j], sd)
+            s += prior_prob[j] * tmp
+            classify_result[j] = tmp
+        
+        for j in range (K):
+            classify_result[j] /= s
+
+        res[i] = np.argmax(classify_result) + 1
+
+    return res
+
+def accuracy (ground_truth, predict):
+    n = len(predict)
+    s = 0.0
+    for i in range (n):
+        if ground_truth[i] == predict[i]:
+            s += 1
+    
+    return s / n
+
+if __name__ == "__main__":
+    K = 20
+    n = 40000
+    p = 5
+    data = generate_data(n, p, K)
+    res = naive_bayes_classification(data, K)
+    print(data[p, :])
+    print(res)
+    print(accuracy(data[p, :], res))
